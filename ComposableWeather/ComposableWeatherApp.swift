ee2c4b78748ace033b39c0c8f7d0a843fa27e941
jhg <kospao7142@gmail.com>
2025-05-22 17:03:16 +0900

    Initial Commit

---
+++ ComposableWeather/ComposableWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComposableWeatherApp.swift
+//  ComposableWeather
+//
+//  Created by Joon Ho Gwak on 5/22/25.
+//
+
+import SwiftUI
+
+@main
+struct ComposableWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
